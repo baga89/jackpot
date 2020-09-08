85328c426d3ac5fc180ae4f0b6b086d0b390f392
Ivan Bagić <ivanbagic89@gmail.com>
2020-09-08 09:43:08 +0200

    Added countUp.js

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+// import { CountUp } from './countUp.js';
+
 gsap.to(".spiral", {duration: 20, rotationZ: 360, transformOrigin:"center", repeat:-1, repeatDelay: 1});
 gsap.from(".coins", 1, {
   y: 10,
@@ -6,28 +8,13 @@ gsap.from(".coins", 1, {
   ease: "power1.inOut",
 });
 
+const options = {
+  decimalPlaces: 2,
+  duration: 10,
+  suffix: 'kn',
+};
 
-Number.prototype.numberFormat = function(decimals, dec_point, thousands_sep) {
-  dec_point = typeof dec_point !== 'undefined' ? dec_point : ',';
-  thousands_sep = typeof thousands_sep !== 'undefined' ? thousands_sep : '.';
-
-  var parts = this.toFixed(decimals).split('.');
-  parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, thousands_sep);
-
-  return parts.join(dec_point);
+window.onload = function() {
+  var countUp = new CountUp('jackpot-numbers', 50000, options);
+  countUp.start();
 }
-
-var counter = { var: 0 };
-
-var tal = document.querySelector(".jackpot-numbers");
-  
-gsap.to(counter, 3, {
-    var: 5000, 
-    onUpdate: function () {
-        var nwc = counter.var.numberFormat(2);
-        tal.innerHTML = nwc;
-    },
-    ease:Circ.easeOut
-});
-
-gsap.from("")
